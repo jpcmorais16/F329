625790594b38cfee7619e1b004e8b51dfaa892cd
jpcmorais16 <jpcmorais2001@gmail.com>
2022-05-01 14:29:12 -0300

    Modificação da função e criação de uma nova

--- RegressaoLinear.py
+++ RegressaoLinear.py
@@ -1,12 +1,22 @@
 import numpy as np
-import pandas as pd
 from matplotlib import pyplot as plt
-import openpyxl
 from scipy import odr
 
-def regLin(x, y, rotulo, nomeArquivoGrafico):
+
+def regLin(x, y):
+    data = odr.RealData(x, y)
+    odreg = odr.ODR(data, odr.models.unilinear)
+    odreg.set_job(fit_type=2)
+    ans = odreg.run()
+
+    a, b = ans.beta
+    da, db = ans.sd_beta
+
+    return a, da, b, db
 
 
+
+def regLinPlot(x, y, nomeArquivoGrafico, xLabel, yLabel, Title):
     data = odr.RealData(x, y)
     odreg = odr.ODR(data, odr.models.unilinear)
     odreg.set_job(fit_type=2)  # muda para mínimos quadrados
@@ -19,18 +29,20 @@ def regLin(x, y, rotulo, nomeArquivoGrafico):
     print(f'coef. linear = ({b}+-{db})')
 
 
-
-    # monta os limites para desenho da reta
     X = np.linspace(min(x), max(x))
     Y = a * X + b
-    # e faz o gráfico dela atrá dos pontos
-    plt.plot(X, Y, color='red', alpha=0.4, label=rotulo)
-    # para exibir as legendas do gráfico
+
+    plt.plot(X, Y, color='red', alpha=0.4)
+
     plt.legend()
 
     plt.scatter(x, y)
 
+    plt.xlabel(xLabel)
+    plt.ylabel(yLabel)
+    plt.title(Title)
+
     plt.savefig(nomeArquivoGrafico)
 
     plt.show()
-    plt.clf()
+    return a, da, b, db
